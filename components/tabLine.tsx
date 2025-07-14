a4169bb506892ab16efe532c3a838ad0c077d6f2
Ph-Dos <phm47860@uga.edu>
2025-07-14 01:41:22 -0400

    feat(receive) Started on the first screen in the receive process tree

---
+++ components/tabLine.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+
+export function TabLine() {
+    return (
+        <View style={{ width: 50, height: 50, alignSelf: 'center', backgroundColor: 'white' }}>
+        </View>
+    );
+}
