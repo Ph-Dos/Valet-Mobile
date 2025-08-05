27f7cf2821e0035a4c2f6c858db4d04f6efc598b
Ph-Dos <phm47860@uga.edu>
2025-08-05 03:23:53 -0400

    fix(receive): fill out data only when phone number is given

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -38,7 +38,7 @@ export default function Receive() {
                 />
                 <Text className="text-[#8D949D]">Guest's mobile number</Text>
             </View>
-            <Pressable onPress={handleTextValue}>
+            <Pressable onPress={() => textValue && handleTextValue()}>
                 <View
                     className="bg-blue-400 rounded-2xl justify-center items-center"
                     style={{ width: 360, height: 50 }}
